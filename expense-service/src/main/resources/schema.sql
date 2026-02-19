aabfe4f609d8d89639f538b5e3b63b6577b99961
Sonia <bhatiamandeep94@gmail.com>
2026-02-19 02:02:20 +0000

    created db

---
+++ expense-service/src/main/resources/schema.sql
@@ -0,0 +1,45 @@
+-- USER TABLE
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+-- ACCOUNT TABLE
+CREATE TABLE account (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) UNIQUE NOT NULL,
+    user_id BIGINT NOT NULL,
+    CONSTRAINT fk_account_user FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+-- TRANSACTION TYPE : INCOME/EXPENSE
+CREATE TYPE transaction_type AS ENUM('INCOME', 'EXPENSE');
+
+-- CATEGORY TABLE
+CREATE TABLE category (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    user_id BIGINT NOT NULL
+);
+
+-- TRANSACTION TABLE
+CREATE TABLE transaction (
+    id SERIAL PRIMARY KEY,
+    amount DECIMAL(15, 2) NOT NULL,
+    description TEXT,
+    transaction_date TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    type transaction_type NOT NULL,
+    account_id BIGINT NOT NULL,
+    category_id BIGINT,
+    user_id NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_transaction_account FOREIGN KEY (account_id) REFERENCES account(id),
+    CONSTRAINT fk_transaction_category FOREIGN KEY (category_id) REFERENCES category(id),
+    CONSTRAINT fk_transaction_user FOREIGN KEY (user_id) REFERENCES users(id),
+    CONSTRAINT ck_amount_positive CHECK (amount>0)
+);
+
+-- Indexes for performance
+CREATE INDEX idx_transactions_user_date ON transaction(user_id, transaction_date DESC);
+CREATE INDEX idx_transactions_account ON transaction(account_id);
