99aea66d760f4ebfa8085a1990773b6c86fc7f8f
Sonia <bhatiamandeep94@gmail.com>
2026-02-23 00:39:58 +0000

    prometheus/grafana

--- expense-service/src/main/java/com/mandyk/expense/config/SecurityConfig.java
+++ expense-service/src/main/java/com/mandyk/expense/config/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig {
         httpSecurity.csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
